e0e7c48004a5c589303f60baae47e108122a5558
Paolo Caldera <paolo.caldera@outlook.com>
2023-05-23 18:05:30 +0200

    Implement Login code (1)

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,17 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.databinding.ActivityRemindersBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,9 +19,14 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+        binding = ActivityAuthenticationBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+
         // TODO: Implement the create account and sign in using FirebaseUI,
         //  use sign in using email and sign in using Google
 
@@ -20,5 +34,46 @@ class AuthenticationActivity : AppCompatActivity() {
 
         // TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+    }
+
+    fun login() {
+        // Enable authentication with both email/password and Google account
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        //val customLayout = AuthMethodPickerLayout.Builder()
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                //.setAuthMethodPickerLayout()
+                .build(),
+            SIGN_IN_REQUEST_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            Log.i("AuthenticationActivity", "Log-in request correctly handled")
+
+            if (resultCode == Activity.RESULT_OK) {
+                val intent = Intent(this, RemindersActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                startActivity(intent)
+            } else {
+                Log.i("AuthenticationActivity", "Log-in FAILED")
+            }
+        } else {
+            Log.i("AuthenticationActivity", "This is not a log-in request")
+        }
+    }
+
+    companion object {
+        private const val SIGN_IN_REQUEST_CODE: Int = 1
     }
 }
---
+++ app/src/main/java/com/udacity/project4/authentication/FirebaseUserLiveData.kt
@@ -0,0 +1,22 @@
+package com.udacity.project4.authentication
+
+import androidx.lifecycle.LiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import com.google.firebase.auth.FirebaseUser
+
+/**
+ * The listener in the class will listen for changes involving users logged-in, users logged-out
+ * or change of the current user
+ */
+class FirebaseUserLiveData : LiveData<FirebaseUser>() {
+
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private val authStateListener = AuthStateListener { value = it.currentUser }
+
+    // Listen to changes happening on the current user
+    override fun onActive() = firebaseAuth.addAuthStateListener(authStateListener)
+
+    // Remove listener when the LiveData is inactive (when activity is stopped or destroyed)
+    override fun onInactive() = firebaseAuth.removeAuthStateListener(authStateListener)
+}
